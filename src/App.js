65f42feee8e90685b376bad7bbd8b7d28015bcdf
Nikil Aadithyan <aadhinikil2003@gmail.com>
2023-12-05 00:24:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Nikil Aadithyan A
+          73772114170
+          computer science and engineering
+          automotive
+           3 rd year
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+function App() {
+return (
+<div className="App">
+<h1>My React App</h1>
+<p>department:CSE</p>
+<p>name:Nikil Aadithyan</p>
+<p>year: 3rd year</P>
+<p>Register number:73772114170</P>
+<p>area of intrest:automotive</p>
+</div>
+);
+}
+export default App;
